9249fee01bae097d5c590fc1e467f0308b0770aa
Matthias Pabel <matthias.pabel@googlemail.com>
2017-09-30 11:01:39 +0200

    entity brick player ball

--- breakout.py
+++ breakout.py
@@ -11,5 +11,36 @@ class Game:
     def update(self):
         pass
 
+class Entity:
+    def __init__(self, pos, width):
+        self.pos = pos
+        self.width = width
+        self.display = np.zeros(shape=(128, 64), dtype=np.int16)
+
+    def render(self):
+        pass
+
+
+
+
+class Player(Entity):
+    def __init__(self, pos, width):
+        super().__init__(pos, width)
+        self.size = np.array((31, 11))
+        self.pos = 64
+
+class Ball(Entity):
+    def __init__(self, pos):
+        super().__init__(pos, 6)
+
+    def render(self):
+        pass
+
+
+class Brick(Entity):
+    def __init__(self, pos, width):
+        super().__init__(pos, width)
+
+
 if __name__ == '__main__':
     Game()
