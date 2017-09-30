e0995abed81c1f962d2e4fa41c906166b9218404
Michael Faath <github@faath.net>
2017-09-30 14:41:46 +0200

    First version of the python lib with tests

---
+++ hackerkoffer-python-lib/hackerkoffer_lib.py
@@ -0,0 +1,128 @@
+# Usage: call start(), overwrite hackerkoffer.callback_* for your own callbacks
+# or keep watch on hackerkoffer.input, .poti and .patchpanel
+import serial
+import threading
+
+
+class Hackerkoffer:
+    SEG7_NUMBERS = [0x7e, 0x48, 0x3d, 0x6d, 0x4b, 0x67, 0x77, 0x4c, 0x7f, 0x6f, 0x5f, 0x73, 0x31, 0x79, 0x37, 0x17]
+    SEG7_DOT = 128
+    SEG7_CLEAR = 0
+
+    def __init__(self):
+        self.ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)  # open serial port
+        print("Serial port opend: " + self.ser.name)
+
+        self.input = []
+        self.poti = []
+        self.patchpanel = []
+
+        for i in range(0,14):
+            self.input.append(False)
+
+        for i in range(0,3):
+            self.poti.append(0)
+
+        for i in range(0,4):
+            self.patchpanel.append(255)
+
+    def callback_inputs(self, id, value):
+        print("Input %d: %d" % (id, value))
+
+    def callback_potis(self, id, value):
+        print("Poti %d: %d" % (id, value))
+
+    def callback_patchpanels(self, id_from, id_to):
+        print("Patchpanel %d -> %d" % (id_from, id_to))
+
+    def _led(self, id, state=0):
+        self.ser.write(b"O %d %d;" % (id, state))
+
+    def led_on(self, id):
+        self._led(id, 1)
+
+    def led_off(self, id):
+        self._led(id, 0)
+
+    # frequency not used yet
+    def _piepser(self, id, state=0, frequency=0):
+        self.ser.write(b"O %d %d;" % (id, state))
+
+    def piepser_on(self, id, frequency=0):
+        self._piepser(id, 1, frequency)
+
+    # frequency not used yet
+    def piepser_off(self, id, frequency=0):
+        self._piepser(id, 0, frequency)
+
+    def _fan(self, id, state=0, frequency=0):
+        self.ser.write(b"O %d %d;" % (id, state))
+
+    def fan_on(self, id, frequency=0):
+        self._fan(id, 1, frequency)
+
+    def fan_off(self, id, frequency=0):
+        self._fan(id, 0, frequency)
+
+    # display
+    def seg7_raw(self, id, raw):
+        self.ser.write(b"c %d %d;" % (id, int(raw)))
+
+    def seg7_number(self, id, number):
+        self.seg7_raw(id, self.SEG7_NUMBERS[number])
+
+
+hackerkoffer = Hackerkoffer()
+
+
+def handle_patchpanel(data):
+    # check, if there are two integers
+    id_from, id_to = data.split(' ')
+    hackerkoffer.patchpanel[int(id_from)] = int(id_to)
+    hackerkoffer.callback_patchpanels(int(id_from), int(id_to))
+
+
+def handle_inputs(data):
+    id, value = data.split(' ')
+    hackerkoffer.input[int(id)] = int(value)
+    hackerkoffer.callback_inputs(int(id), int(value)==1)
+
+
+def handle_potis(data):
+    id, value = data.split(' ')
+    hackerkoffer.poti[int(id)] = int(value)
+    hackerkoffer.callback_potis(int(id), int(value))
+
+
+def handle_data(data):
+    if len(data) <= 0:
+        return
+
+    typ = data[0]
+    data = data[2:]
+
+    if typ == 'E':
+        print("Error: %s" % data)
+    elif typ == 'P':
+        handle_patchpanel(data)
+    elif typ == 'I':
+        handle_inputs(data)
+    elif typ == 'A':
+        handle_potis(data)
+
+
+def read_from_port(ser):
+    buf = ''
+    while True:
+        reading = ser.readline().decode()
+        buf = buf + reading
+        parts = buf.split(';')
+        for part in parts[:-1]:
+            handle_data(part)
+        buf = parts[-1]
+
+
+def start():
+    # background thread to read from device
+    thread = threading.Thread(target=read_from_port, args=(hackerkoffer.ser,))
+    thread.start()
---
+++ hackerkoffer-python-lib/tests.py
@@ -0,0 +1,108 @@
+from hackerkoffer_lib import start, hackerkoffer
+import time
+
+def test_all_leds():
+    print("BEGINNING LED TEST")
+
+    for i in range(0,10):
+        print("LED %i" %i)
+        hackerkoffer.led_on(i)
+        time.sleep(0.5)
+        hackerkoffer.led_off(i)
+
+    print("END OF LED TEST")
+
+
+def test_all_piepser():
+    print("BEGINNING PIEPSER TEST (HIGH/LOW)")
+
+    for i in (15,16):
+        hackerkoffer.piepser_on(i)
+        time.sleep(1)
+        hackerkoffer.piepser_off(i)
+
+    print("END OF PIEPSER TEST")
+
+
+def test_fan():
+    print("BEGINNING FAN TEST")
+    hackerkoffer.fan_on(17)
+    time.sleep(3)
+    hackerkoffer.fan_off(17)
+    print("END OF FAN TEST")
+
+
+def test_seg7():
+    print("BEGINNING OF SEG7 TEST (RIGHT TO LEFT)")
+    for segment in range(0,3):
+        for i in range(0, 9):
+            hackerkoffer.seg7_number(segment, i)
+            time.sleep(0.5)
+            hackerkoffer.seg7_raw(segment, hackerkoffer.SEG7_CLEAR)
+
+        time.sleep(2)
+
+    print("END OF SEG7 TEST")
+
+
+def wait_on_input(id, state):
+    while True:
+        if hackerkoffer.input[id] == state:
+            return
+        time.sleep(1)
+
+
+def wait_on_poti_min(id):
+    while True:
+        if hackerkoffer.poti[id] <= 10:
+            return
+        time.sleep(1)
+
+
+def wait_on_poti_max(id):
+    while True:
+        if hackerkoffer.poti[id] >= 4086:
+            return
+        time.sleep(1)
+
+
+def test_inputs():
+    for i in range(0,14):
+        print("Flip switch %d on" % i)
+        wait_on_input(i, True)
+        print("Flip switch %d off" % i)
+        wait_on_input(i, False)
+
+
+def test_potis():
+    for i in range(0,3):
+        print("Turn poti %d to min" % i)
+        wait_on_poti_min(i)
+        print("Turn poti %d to max" % i)
+        wait_on_poti_max(i)
+
+
+def test_all():
+    # print("BEGINNING OUTPUT TESTS")
+    # test_all_leds()
+    # time.sleep(2)
+    # test_all_piepser()
+    # time.sleep(2)
+    # test_seg7()
+    # time.sleep(2)
+    # test_fan()
+    #
+    # print("BEGINNING INPUT TESTS")
+    test_inputs()
+    test_potis()
+
+    print("TEST SUCCESSFUL, BOOYAH!")
+
+def main():
+    start()
+
+    test_all()
+
+
+if __name__ == "__main__":
+    main()
