a2caba58e01cba4df9ab6e30613f0b8aae586dbc
leon <leon.koenekamp@gmail.com>
2017-09-30 10:37:21 +0200

    init breakout

---
+++ breakout.py
@@ -0,0 +1,15 @@
+
+import numpy as np
+
+class Game:
+    def __init__(self):
+        self.display = np.zeros(shape=(128, 64), dtype=np.int16)r
+        self.player =
+
+
+
+    def update(self):
+        pass
+
+if __name__ == '__main__':
+    Game()
